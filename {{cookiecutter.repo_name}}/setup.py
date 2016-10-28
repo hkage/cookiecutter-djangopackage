c789edefd86763ee4d1f62c7f3fcc25a7e82f735
Henning Kage <henning.kage@gmail.com>
2016-10-28 11:29:29 +0200

    Changed setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -6,50 +6,59 @@ from setuptools import setup, find_packages
 
 exec(open('app_name/version.py').read())
 
+classifiers = """
+'Development Status :: 3 - Alpha',
+'Framework :: Django',{% if '1.7' in cookiecutter.django_versions %}
+'Framework :: Django :: 1.7',{% endif %}{% if '1.8' in cookiecutter.django_versions %}
+'Framework :: Django :: 1.8',{% endif %}{% if '1.9' in cookiecutter.django_versions %}
+'Framework :: Django :: 1.9',{% endif %}{% if '1.10' in cookiecutter.django_versions %}
+'Framework :: Django :: 1.10',{% endif %}
+'Intended Audience :: Developers',
+'License :: OSI Approved :: BSD License',
+'Natural Language :: English',
+'Programming Language :: Python :: 2',
+'Programming Language :: Python :: 2.7',
+'Programming Language :: Python :: 3',{% if '1.7' in cookiecutter.django_versions or '1.8' in cookiecutter.django_versions %}
+'Programming Language :: Python :: 3.2',
+'Programming Language :: Python :: 3.3',{% endif %}
+'Programming Language :: Python :: 3.4',{% if '1.7' != cookiecutter.django_versions %}
+'Programming Language :: Python :: 3.5',{% endif %}
+"""
+
 tests_require = [
+    'coverage',
+    'flake8',
+    'pydocstyle',
+    'pylint',
+    'pytest-django',
     'pytest-pep8',
     'pytest-cov',
-    'pytest-django'
     # for pytest-runner to work, it is important that pytest comes last in
     # this list: https://github.com/pytest-dev/pytest-runner/issues/11
     'pytest',
 ]
 
+install_requires = [
+    # 'six',
+]
+
 setup(name='{{ cookiecutter.repo_name }}',
-    version=__version__,
-    description="""{{ cookiecutter.project_short_description }}""",,
-    long_description="""""",
-    author='{{ cookiecutter.full_name }}',
-    author_email='{{ cookiecutter.email }}',
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django',{% if '1.7' in cookiecutter.django_versions %}
-        'Framework :: Django :: 1.7',{% endif %}{% if '1.8' in cookiecutter.django_versions %}
-        'Framework :: Django :: 1.8',{% endif %}{% if '1.9' in cookiecutter.django_versions %}
-        'Framework :: Django :: 1.9',{% endif %}{% if '1.10' in cookiecutter.django_versions %}
-        'Framework :: Django :: 1.10',{% endif %}
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',{% if '1.7' in cookiecutter.django_versions or '1.8' in cookiecutter.django_versions %}
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',{% endif %}
-        'Programming Language :: Python :: 3.4',{% if '1.7' != cookiecutter.django_versions %}
-        'Programming Language :: Python :: 3.5',{% endif %}
-    ],
-    license='{{ cookiecutter.open_source_license }}',
-    keywords='{{ cookiecutter.repo_name }}',
-    packages=find_packages(exclude=['tests']),
-    include_package_data=True,
-    setup_requires=[
-        'pytest-runner',
-    ],
-    install_requires=[
-        'Django>=1.6.0'
-    ],
-    tests_require=tests_require,
-    extras_require={'test': tests_require}
-    )
+      version=__version__,  # noqa
+      description="""{{ cookiecutter.project_short_description }}""",
+      long_description="""TODO:""",
+      author='{{ cookiecutter.author_name }}',
+      author_email='{{ cookiecutter.email }}',
+      url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
+      classifiers=[c.strip() for c in classifiers.splitlines()
+                   if c.strip() and not c.startswith('#')],
+      license='{{ cookiecutter.open_source_license }}',
+      keywords='{{ cookiecutter.repo_name }}',
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      test_suite='tests',
+      setup_requires=[
+          'pytest-runner',
+          ],
+      install_requires=install_requires,
+      tests_require=tests_require,
+      )

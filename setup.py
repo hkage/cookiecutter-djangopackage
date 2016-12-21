58d782ccf3549d46b0fddd4f8ad2fc2ebd8f4009
Henning Kage <henning.kage@gmail.com>
2016-12-21 10:06:06 +0100

    Added dev files

---
+++ setup.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cookiecutter-djangopackage',
+    packages=[],
+    version='0.1.1',
+    description='Cookiecutter template for Tornado based projects',
+    author='Henning Kage',
+    author_email='henning.kage@gmail.com',
+    url='https://github.com/hkage/cookiecutter-djangopackage',
+    keywords=['cookiecutter', 'template', 'django', ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development',
+    ],
+)
---
+++ tests/test_bake_project.py
@@ -0,0 +1,26 @@
+"""Tests for the cookiecutter template."""
+
+from contextlib import contextmanager
+
+from cookiecutter.utils import rmtree
+
+
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project))
+
+
+def test_bake_project_with_defaults(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.project.isdir()
+        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        assert 'setup.py' in found_toplevel_files
+        assert 'tox.ini' in found_toplevel_files
+        assert 'Dockerfile' in found_toplevel_files
+        assert 'tests' in found_toplevel_files
